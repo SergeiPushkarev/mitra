05e2046b59ab96e4a973288a6cfe30456a25ca66
SergeiPushkarev <87870860+SergeiPushkarev@users.noreply.github.com>
2023-07-05 20:22:42 +0300

    Добавлен поиск по заголовку поста

--- src/components/UI/SearchPost.jsx
+++ src/components/UI/SearchPost.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
 
-const SearchPost = () => {
+const SearchPost = ({search,setSearchQuery}) => {
   return (
     <div>
         <Form.Label>Search</Form.Label>
         <Form.Control
+            value={search}
             className="form-control"
             type="search"
             id="example-search-input"
+            onChange={event=>setSearchQuery(event.target.value)}
         />
     </div>
   )
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState} from 'react'
 import { Container } from 'react-bootstrap'
 import PostList from '../components/PostList'
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,18 +8,30 @@ import SearchPost from '../components/UI/SearchPost';
 
 
 const Posts = () => {
+    
     const posts = useSelector(state => state.posts.posts)
     const isLoading = useSelector(state => state.posts.isLoading)
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(fetchPostsAction())
     }, [dispatch])
+
+    const [searchQuery, setSearchQuery] = useState('')
+
+    const useVisiblePosts = (posts, searchQuery) => {
+        const searchedPosts = useMemo(() =>{
+            return posts.filter(a=> a.title.toLocaleLowerCase().includes(searchQuery))
+        }, [posts, searchQuery])
+        return searchedPosts
+    }
+    const visiblePosts = useVisiblePosts(posts,searchQuery)
+    console.log(searchQuery);
   return (
     <Container className="d-flex flex-column justify-content-center" style={{ maxWidth: '900px' }}>
-        <SearchPost/>
+        <SearchPost searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
         {isLoading
         ? <Loader/>
-        : <PostList posts={posts}/>
+        : <PostList posts={visiblePosts}/>
         } 
     </Container>
   )
